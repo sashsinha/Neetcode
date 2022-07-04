4001bb422ffd20e22597e2727ffd08e8104f5f41
Sash Sinha <shash678@outlook.com>
2022-07-04 18:05:35 -0400

    36. Valid Sudoku

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,30 +1,24 @@
 import collections
 
 class Solution:
-    def check_valid_nine_cells(self, nine_cells: list[str]) -> bool:
-        counts = collections.Counter(nine_cells)
-        for k, v in counts.items():
-            if v > 1 and k != '.':
-                return False
-        return True
-    
     def isValidSudoku(self, board: list[list[str]]) -> bool:
-        sub_boxes = defaultdict(list)
-        cols = defaultdict(list)
-
-        for row_idx, row in enumerate(board):
-            if not self.check_valid_nine_cells(row):
-                return False
-            for col_idx, val in enumerate(row):
-                cols[col_idx].append(val)
-                sub_boxes[(row_idx // 3, col_idx // 3)].append(val)
-  
-        for col in cols.values():
-            if not self.check_valid_nine_cells(col):
-                return False
- 
-        for sub_box in sub_boxes.values():
-            if not self.check_valid_nine_cells(sub_box):
-                return False
-
+        n = 9
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        sub_boxes = collections.defaultdict(set)
+        for r in range(n):
+            for c in range(n):
+                val = board[r][c]
+                if val == '.':
+                    continue
+                if val in rows[r]:
+                    return False
+                rows[r].add(val)
+                if val in cols[c]:
+                    return False
+                cols[c].add(val)
+                sub_box_coords = (r // 3, c // 3)
+                if val in sub_boxes[sub_box_coords]:
+                    return False
+                sub_boxes[sub_box_coords].add(val)
         return True
