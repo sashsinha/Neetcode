3880a687550b3a8194bd06d4bb1fe0bc5f0c7bfc
Sash Sinha <shash678@outlook.com>
2022-07-04 16:43:36 -0400

    49. Group Anagrams

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,8 +1,14 @@
 import collections
 
 class Solution:
+    def get_char_counts_string(self, s: str) -> tuple[int, ...]:
+        char_counts = [0] * 26
+        for ch in s:
+            char_counts[ord(ch) - ord('a')] += 1
+        return tuple(char_counts)
+    
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         groups = collections.defaultdict(list)
         for s in strs:
-            groups[''.join(sorted(s))].append(s)
+            groups[self.get_char_counts_string(s)].append(s)
         return groups.values()
