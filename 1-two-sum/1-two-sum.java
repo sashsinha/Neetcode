a4f7dde641a4042e3ecfdeddc5e54ab6e4ed95a4
Sash Sinha <shash678@outlook.com>
2022-07-04 16:24:34 -0400

    1. Two Sum

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -6,7 +6,7 @@ class Solution {
         Map<Integer, Integer> numToComplimentIdx = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (numToComplimentIdx.containsKey(nums[i])) {
-                return new int[] {numToComplimentIdx.get(nums[i]), i};
+                return new int[]{numToComplimentIdx.get(nums[i]), i};
             }   
             numToComplimentIdx.put(target - nums[i], i);
         }
