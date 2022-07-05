99f888fed65b22d5b207eac42d13fd7193ed9527
Sash Sinha <shash678@outlook.com>
2022-07-04 21:33:04 -0400

    252. Meeting Rooms

--- 252-meeting-rooms/252-meeting-rooms.py
+++ 252-meeting-rooms/252-meeting-rooms.py
@@ -1,6 +1,6 @@
 class Solution:
     def canAttendMeetings(self, intervals: list[list[int]]) -> bool:
-        intervals.sort(key=lambda i: (i[0], i[1]))
+        intervals.sort(key=lambda i: i[0])
         for prev, curr in zip(intervals[:-1], intervals[1:]):
             if curr[0] < prev[1]:
                 return False
