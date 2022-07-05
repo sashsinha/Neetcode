90a89fae9d45e1be2ec10751bb99d8dc216da484
Sash Sinha <shash678@outlook.com>
2022-07-04 20:59:39 -0400

    141. Linked List Cycle

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -6,6 +6,8 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
         slow, fast = head, head
         while fast and fast.next:
             slow = slow.next
