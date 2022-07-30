4c3354b680998b22d9aa3ff296849186c373d466
Sash Sinha <shash678@outlook.com>
2022-07-30 09:30:17 -0400

    20. Valid Parentheses

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,15 +1,16 @@
 class Solution:
+    OPENING_TO_CLOSING = {'[': ']', '(': ')', '{': '}'}  # Should use immutabledict
+    CLOSING_TO_OPENING = {']': '[', ')': '(', '}': '{'}  # Should use immutabledict
+    
     def isValid(self, s: str) -> bool:
-        closing_to_opening = {
-            ')': '(',
-            '}': '{',
-            ']': '[',
-        }
+        if not s:
+            return True
         stack = []
         for ch in s:
-            if ch not in closing_to_opening:
+            if ch in Solution.OPENING_TO_CLOSING:
                 stack.append(ch)
-            else:
-                if not stack or closing_to_opening[ch] != stack.pop():
+            elif ch in Solution.CLOSING_TO_OPENING:
+                if not stack or stack[-1] != Solution.CLOSING_TO_OPENING[ch]:
                     return False
+                stack.pop()
         return not stack
